021266a614e689734c07f9df9064ce69f6332b31
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-10-09 15:50:23 +0700

    optimize code

--- two-fer/two_fer.cpp
+++ two-fer/two_fer.cpp
@@ -1,16 +1,8 @@
 #include "two_fer.h"
-#include <string>
-
-using namespace std;
 
 namespace two_fer
 {
-    string two_fer() {
-        return "One for you, one for me.";
-    }
-
-    string two_fer(string name) {
+    std::string two_fer(std::string name) {
         return "One for " + name + ", one for me.";
     }
 } // namespace two_fer
-
--- two-fer/two_fer.h
+++ two-fer/two_fer.h
@@ -1,13 +1,10 @@
-#include <string>
 #if !defined(TWO_FER_H)
 #define TWO_FER_H
-
-using namespace std;
+#include <string>
 
 namespace two_fer
 {
-    string two_fer();
-    string two_fer(string);
+    std::string two_fer(std::string name="you");
 } // namespace two_fer
 
 #endif //TWO_FER_H
